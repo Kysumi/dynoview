8dd235745ce04d706798f87b5f9ea8ccc14ea18f
Maunder <maunderscott@gmail.com>
2024-10-13 16:31:41 +1300

    Adding scan

--- src/preload/ddb/operations/index.ts
+++ src/preload/ddb/operations/index.ts
@@ -1,3 +1,4 @@
 export * from "./list-available-tables";
 export * from "./get-table-information";
 export * from "./query-table-index";
+export * from "./scan-table";
--- src/renderer/src/components/QueryBuilder/Scan.tsx
+++ src/renderer/src/components/QueryBuilder/Scan.tsx
@@ -1,3 +1,39 @@
+import { Button } from "@nextui-org/react";
+import useTableStore from "@renderer/store";
+import { useState } from "react";
+
 export const Scan = () => {
-  return <div>Scan</div>;
+  const [result, setResult] = useState<any>(null);
+  const { activeAWSRegion, activeTable } = useTableStore();
+
+  if (!activeTable) return null;
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.currentTarget);
+
+    const limit = formData.get("limit") as string;
+
+    const result = await window.api.scanTable({
+      region: activeAWSRegion,
+      tableName: activeTable.tableName,
+      limit: Number.parseInt(limit),
+    });
+
+    console.log(result);
+
+    setResult(result);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="number" name="limit" value={50} hidden />
+        <Button type="submit">Scan</Button>
+      </form>
+
+      <pre>{JSON.stringify(result, null, 2)}</pre>
+    </div>
+  );
 };
