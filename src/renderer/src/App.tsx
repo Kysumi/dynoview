03406df70e3f652c4a13cd814c11ab9ed22199c7
Maunder <maunderscott@gmail.com>
2024-10-13 15:19:13 +1300

    Moving stuff around

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,79 +1,14 @@
 import Versions from "./components/Versions";
-import { useState, useEffect } from "react";
+import LeftNav from "./LeftNav/LeftNav";
 import useTableStore from "./store";
-import { Autocomplete, AutocompleteItem, Button } from "@nextui-org/react";
-import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle, SheetTrigger } from "./components/Sheet";
-import { regions } from "../../preload/ddb/config";
 
 function App(): JSX.Element {
-  const [tables, setTables] = useState<string[]>([]);
-  const { activeTable, setActiveTable, activeAWSRegion, setAWSRegion } = useTableStore();
-
-  const listTables = async () => {
-    const tables = await window.api.listAvailableTables({ region: activeAWSRegion });
-    return tables;
-  };
-
-  useEffect(() => {
-    listTables().then((tables) => setTables(tables));
-  }, [activeAWSRegion]);
+  const { activeTable } = useTableStore();
 
   return (
     <div>
       <div className="h-16 bg-gray-100">
-        <div className="flex items-center px-4">
-          <Sheet modal={false}>
-            <SheetTrigger asChild>
-              <Button>Open</Button>
-            </SheetTrigger>
-            <SheetContent side="left">
-              <SheetHeader>
-                <SheetTitle>Configuration</SheetTitle>
-                <SheetDescription>Change Paramaters</SheetDescription>
-                <Autocomplete
-                  label="Active Table"
-                  className="max-w-xs"
-                  isClearable={false}
-                  selectedKey={activeTable?.tableName ?? null}
-                  onSelectionChange={async (e) => {
-                    if (e?.toString()) {
-                      const info = await window.api.getTableInformation({
-                        tableName: e.toString(),
-                        region: activeAWSRegion,
-                      });
-                      setActiveTable(info);
-                    }
-                  }}
-                >
-                  {tables.map((table) => (
-                    <AutocompleteItem key={table} value={table}>
-                      {table}
-                    </AutocompleteItem>
-                  ))}
-                </Autocomplete>
-
-                <Autocomplete
-                  label="AWS Region"
-                  className="max-w-xs"
-                  isClearable={false}
-                  selectedKey={activeAWSRegion}
-                  onSelectionChange={(e) => {
-                    if (e?.toString()) {
-                      setActiveTable(undefined);
-                      setAWSRegion(e.toString());
-                    }
-                  }}
-                >
-                  {regions.map((region) => (
-                    <AutocompleteItem key={region} value={region}>
-                      {region}
-                    </AutocompleteItem>
-                  ))}
-                </Autocomplete>
-              </SheetHeader>
-            </SheetContent>
-          </Sheet>
-        </div>
+        <LeftNav />
       </div>
       <div>
         <div className="flex flex-col gap-2">
---
+++ src/renderer/src/LeftNav/LeftNav.tsx
@@ -0,0 +1,86 @@
+import { Autocomplete, AutocompleteItem, Button } from "@nextui-org/react";
+import { Database } from "lucide-react";
+import useTableStore from "../store";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@renderer/components/Sheet";
+import { useEffect, useState } from "react";
+import { regions } from "@shared/available-regions";
+
+export default function LeftNav() {
+  const [tables, setTables] = useState<string[]>([]);
+  const { activeTable, setActiveTable, activeAWSRegion, setAWSRegion } = useTableStore();
+
+  const listTables = async () => {
+    const tables = await window.api.listAvailableTables({ region: activeAWSRegion });
+    return tables;
+  };
+
+  useEffect(() => {
+    listTables().then((tables) => setTables(tables));
+  }, [activeAWSRegion]);
+
+  return (
+    <div className="flex items-center px-4">
+      <Sheet modal={false}>
+        <SheetTrigger asChild>
+          <Button>
+            <Database />
+            {activeTable?.tableName ?? "Select Table"}
+          </Button>
+        </SheetTrigger>
+        <SheetContent side="left">
+          <SheetHeader>
+            <SheetTitle>Configuration</SheetTitle>
+            <SheetDescription>Change Paramaters</SheetDescription>
+            <Autocomplete
+              label="Active Table"
+              className="max-w-xs"
+              isClearable={false}
+              selectedKey={activeTable?.tableName ?? null}
+              onSelectionChange={async (e) => {
+                if (e?.toString()) {
+                  const info = await window.api.getTableInformation({
+                    tableName: e.toString(),
+                    region: activeAWSRegion,
+                  });
+                  setActiveTable(info);
+                }
+              }}
+            >
+              {tables.map((table) => (
+                <AutocompleteItem key={table} value={table}>
+                  {table}
+                </AutocompleteItem>
+              ))}
+            </Autocomplete>
+
+            <Autocomplete
+              label="AWS Region"
+              className="max-w-xs"
+              isClearable={false}
+              selectedKey={activeAWSRegion}
+              onSelectionChange={(e) => {
+                if (e?.toString()) {
+                  setActiveTable(undefined);
+                  setAWSRegion(e.toString());
+                }
+              }}
+            >
+              {regions.map((region) => (
+                <AutocompleteItem key={region} value={region}>
+                  {region}
+                </AutocompleteItem>
+              ))}
+            </Autocomplete>
+          </SheetHeader>
+        </SheetContent>
+      </Sheet>
+    </div>
+  );
+}
