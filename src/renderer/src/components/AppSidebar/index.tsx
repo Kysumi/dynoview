09d9f90464ddbc6cb86eb4afc5dd9b49510bdea6
Maunder <maunderscott@gmail.com>
2024-11-16 19:49:18 +1300

    Sidebar improvements

--- src/renderer/src/components/AppSidebar/index.tsx
+++ src/renderer/src/components/AppSidebar/index.tsx
@@ -1,12 +1,57 @@
-import { SidebarContent, SidebarGroup, SidebarFooter, Sidebar, SidebarHeader } from "../SideBar";
+import { Database, User, Settings } from "lucide-react";
+import {
+  SidebarContent,
+  SidebarGroup,
+  SidebarFooter,
+  Sidebar,
+  SidebarHeader,
+  SidebarGroupContent,
+  SidebarMenu,
+  SidebarMenuItem,
+  SidebarGroupLabel,
+  SidebarMenuButton,
+} from "../SideBar";
+
+const items = [
+  {
+    title: "Dashboard",
+    url: "/dashboard",
+    icon: Database,
+  },
+  {
+    title: "Users",
+    url: "/users",
+    icon: User,
+  },
+  {
+    title: "Settings",
+    url: "/settings",
+    icon: Settings,
+  },
+];
 
 export const AppSidebar = () => {
   return (
     <Sidebar>
-      <SidebarHeader />
+      <SidebarHeader>DynoView</SidebarHeader>
       <SidebarContent>
-        <SidebarGroup />
-        <SidebarGroup />
+        <SidebarGroup>
+          <SidebarGroupLabel>Application</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
       </SidebarContent>
       <SidebarFooter />
     </Sidebar>
